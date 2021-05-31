e190cf081df507b5a421832f7dc10893464c2ffd
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-05-31 22:04:48 +0700

    Operasi String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Fauzi";
@@ -12,10 +15,23 @@ public class Main {
 
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
-        System.out.println(uniskaString);
 
         String namaLengkap = namaDepan;
+        System.out.println(namaLengkap);
         System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("zi"));
+        System.out.println(namaLengkap.substring(2));
+        System.out.println(namaLengkap.substring(2,4));
+        System.out.println(namaLengkap.replace("Fauzi", "Barr"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+
+        String namaArray[] = namaLengkap.split("zi");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
+
 
     }
 }
